d2870add18ad9444fe6ee407aff6da831a427fea
Oscar Moreno Sancho <omoreno-@cbr11s5.42barcelona.com>
2023-10-02 18:21:42 +0200

    ex00 refactor

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: omoreno- <omoreno-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 16:13:45 by omoreno-          #+#    #+#             */
-/*   Updated: 2023/09/29 18:49:23 by omoreno-         ###   ########.fr       */
+/*   Updated: 2023/10/02 18:19:44 by omoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,109 +33,276 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& b)
 	return (*this);
 }
 
-void	ScalarConverter::convert_char(char value)
+std::string	ScalarConverter::trim(std::string value)
 {
-	int		iValue = 0;
-	float	fValue = 0.0f;
-	double	dValue = 0.0;
+	std::string res = "";
+	size_t start = 0;
+	size_t end = value.length();
 
-	if (value < 32 || value > 126)
+	if (end == 0)
+		return (res);
+	else
+		end--;
+	while (value[start] && start < end && value[start] <= ' ')
+		start++;
+	while (end >= 0 && start < end && value[end] <= ' ')
+		end--;
+	for (size_t i = start; i <= end; i++)
+		res += value[i];
+	return (res);
+}
+
+ScalarConverter::t_counts	ScalarConverter::counts(std::string value)
+{
+	ScalarConverter::t_counts	counts;
+	size_t						i;
+	size_t						len = value.length();
+
+	counts.alphas = 0;
+	counts.digits = 0;
+	counts.exps = 0;
+	counts.floats = 0;
+	counts.signs = 0;
+	counts.spaces = 0;
+	counts.dots = 0;
+	for (i = 0; i < len; i++)
+	{
+		char c = value[i];
+		if (c <= ' ')
+		{
+			counts.spaces++;
+			continue;
+		}
+		if (c > '0' && c < '9')
+		{
+			counts.digits++;
+			continue;
+		}
+		if (c == 'e' || c == 'E')
+		{
+			counts.exps++;
+			continue;
+		}
+		if (c == 'f' || c == 'F')
+		{
+			counts.floats++;
+			continue;
+		}
+		if (c == '+' || c == '-')
+		{
+			counts.signs++;
+			continue;
+		}
+		if (c == '.')
+		{
+			counts.dots++;
+			continue;
+		}
+		if ((c > 'A' && c < 'Z') || (c > 'a' && c < 'z'))
+		{
+			counts.alphas++;
+			continue;
+		}
+	}
+	return (counts);
+}
+
+
+ScalarConverter::t_type	ScalarConverter::identify(std::string value)
+{
+	if (value.length() == 1)
+		return (TYPE_CHAR);
+	t_counts counted = counts(value);
+	if (counted.alphas == 0 && counted.exps == 0 &&
+		counted.floats == 0 && counted.spaces == 0 && counted.digits > 0
+		&& counted.dots == 0 && (counted.signs == 0 ||
+		(counted.signs == 1 && (value[0] == '-' || value[0] == '+'))))
+		return (TYPE_INT);
+	if (counted.alphas == 0 && counted.exps < 2 && counted.floats == 1 && 
+		(value[value.length()-1] == 'f' || (value[value.length()-1] == 'F'))
+		&& counted.signs < 3 && counted.dots < 2 && counted.spaces == 0)
+		return (TYPE_FLOAT);
+	if (counted.alphas == 0 && counted.exps < 2 &&
+		counted.floats == 0 && counted.signs < 3 &&
+		counted.dots < 2 && counted.spaces == 0)
+		return (TYPE_DOUBLE);
+	return (TYPE_INVALID);
+}
+
+void	ScalarConverter::print_char(char cV)
+{
+	if (cV > ' ')
+		std::cout << "char: " << cV << std::endl;
+	else
 		std::cout << "char: " << NON_DISPLAY << std::endl;
-	else
-		std::cout << "char: '" << value << "'" << std::endl;
-	iValue = static_cast<int>(value);
-	fValue = static_cast<float>(iValue);
-	dValue = static_cast<double>(iValue);
-	std::cout.setf(std::ios::fixed);
-	std::cout.setf(std::ios::showpoint);
-	std::cout << std::setprecision(1);
-	std::cout << "int: " << std::setw(20) << iValue << std::endl;
-	std::cout << "float: " << std::setw(20) << fValue << "f" << std::endl;
-	std::cout << "double: " << std::setw(20) << dValue << std::endl;
 }
 
-bool ScalarConverter::convert_to_int(int *i, std::string value)
+void			ScalarConverter::print_int(int iV)
 {
-	try
-	{
-		*i = std::stoi(value);
-		return (false);
-	}
-	catch(const std::exception& e)
-	{
-		*i = 0;
-		return (true);
-	}
+	std::cout << "int: " << iV << std::endl;
 }
 
-bool ScalarConverter::convert_to_double(double *d, std::string value)
+void			ScalarConverter::print_float(float fV)
 {
-	try
+	if (fV >= 1.0 && fV < 9.0e6)
 	{
-		*d = std::stod(value);
-		return (false);
-	}
-	catch(const std::exception& e)
-	{
-		*d = 0.0;
-		return (true);
-	}
-}
-
-void	ScalarConverter::convert_not_char(std::string value)
-{
-	int		iValue = 0;
-	float	fValue = 0.0f;
-	double	dValue = 0.0;
-	char	cValue = 0;
-	bool	iFault = false;
-	bool	dFault = false;
-
-	dFault = convert_to_double(&dValue, value);
-	if (dFault)
-	{
-		std::cout << "char: " << IMPOSSIBLE << std::endl;
-		std::cout << "int: " << IMPOSSIBLE << std::endl;
-		std::cout << "float: " << IMPOSSIBLE << std::endl;
-		std::cout << "double: " << IMPOSSIBLE << std::endl;
-		return ;
-	}
-	fValue = static_cast<float>(dValue);
-	iFault = convert_to_int(&iValue, value);
-	if (iFault)
-	{
-		std::cout << "char: " << IMPOSSIBLE << std::endl;
-		std::cout << "int: " << IMPOSSIBLE << std::endl;
-		std::cout.setf(std::ios::fixed);
 		std::cout.setf(std::ios::showpoint);
-		std::cout << "float: " << std::setprecision(1) << fValue << "f" << std::endl;
-		std::cout << "double: " << std::setprecision(1) << dValue << std::endl;
-		return ;
-	}
-	if (dValue >= static_cast<double>(-2147483648) && dValue <= static_cast<double>(2147483647))
-	{
-		iValue = static_cast<int>(dValue);
-		cValue = static_cast<char>(iValue);
-		if (iValue < 32 || iValue > 126)
-			std::cout << "char: " << NON_DISPLAY << std::endl;
-		else
-			std::cout << "char: '" << cValue << "'" << std::endl;
-		std::cout << "int: " << iValue << std::endl;
+		std::cout.setf(std::ios::fixed);
+		std::cout << std::setprecision(1);
 	}
 	else
+		std::cout << std::setprecision(6);
+	std::cout << "float: " << fV << "f" << std::endl;
+}
+
+void			ScalarConverter::print_double(double dV)
+{
+	if (dV >= 1.0 && dV < 9.0e15)
 	{
-		std::cout << "char: " << IMPOSSIBLE << std::endl;
-		std::cout << "int: " << IMPOSSIBLE << std::endl;
+		std::cout.setf(std::ios::showpoint); 
+		std::cout.setf(std::ios::fixed);
+		std::cout << std::setprecision(1);
 	}
-	std::cout.setf(std::ios::fixed);
-	std::cout.setf(std::ios::showpoint);
-	std::cout << "float: " << std::setprecision(1) << fValue << "f" << std::endl;
-	std::cout << "double: " << std::setprecision(1) << dValue << std::endl;
+	else
+		std::cout << std::setprecision(15);
+	std::cout << "double: " << dV << std::endl;
+}
+
+void	ScalarConverter::print_all_impossible()
+{
+	std::cout << "char: " << IMPOSSIBLE << std::endl;
+	std::cout << "int: " << IMPOSSIBLE << std::endl;
+	std::cout << "float: " << IMPOSSIBLE << std::endl;
+	std::cout << "double: " << IMPOSSIBLE << std::endl;
+}
+
+void	ScalarConverter::show_char(std::string trimmed)
+{
+	char	cV;
+	int		iV;
+	float	fV;
+	double	dV;
+	cV = trimmed[0];
+	iV = static_cast<int>(cV);
+	fV = static_cast<float>(cV);
+	dV = static_cast<double>(cV);
+	// std::cout << "identified as char: \n";
+	print_char(cV);
+	print_int(iV);
+	print_float(fV);
+	print_double(dV);
+}
+
+void	ScalarConverter::show_int(std::string trimmed)
+{
+	char	cV;
+	int		iV;
+	float	fV;
+	double	dV;
+	try
+	{
+		iV = std::stoi(trimmed);
+	}
+	catch(const std::exception& e)
+	{
+		print_all_impossible();
+		return ;
+	}
+	cV = static_cast<char>(iV);
+	fV = static_cast<float>(iV);
+	dV = static_cast<double>(iV);
+	// std::cout << "identified as int: \n";
+	if (iV < 0 || iV > 255)
+		std::cout << "char: " << IMPOSSIBLE << std::endl;
+	else
+		print_char(cV);
+	print_int(iV);
+	print_float(fV);
+	print_double(dV);
+}
+
+void	ScalarConverter::show_float(std::string trimmed)
+{
+	char	cV;
+	int		iV;
+	float	fV;
+	double	dV;
+	try
+	{
+		fV = std::stof(trimmed);
+	}
+	catch(const std::exception& e)
+	{
+		print_all_impossible();
+		return ;
+	}
+	cV = static_cast<char>(fV);
+	iV = static_cast<int>(fV);
+	dV = static_cast<double>(fV);
+	// std::cout << "identified as float: \n";
+	if (fV < 0.0f || fV > 255.0f)
+		std::cout << "char: " << IMPOSSIBLE << std::endl;
+	else
+		print_char(cV);
+	if (fV < -2147483648.0 || fV > 2147483647.0)
+		std::cout << "int: " << IMPOSSIBLE << std::endl;
+	else
+		print_int(iV);
+	print_float(fV);
+	print_double(dV);
+}
+
+void	ScalarConverter::show_double(std::string trimmed)
+{
+	char	cV;
+	int		iV;
+	float	fV;
+	double	dV;
+	try
+	{
+		dV = std::stod(trimmed);
+	}
+	catch(const std::exception& e)
+	{
+		print_all_impossible();
+		return ;
+	}
+	cV = static_cast<char>(dV);
+	iV = static_cast<int>(dV);
+	fV = static_cast<float>(dV);
+	// std::cout << "identified as double: \n";
+	if (dV < 0.0 || dV > 255.0)
+		std::cout << "char: " << IMPOSSIBLE << std::endl;
+	else
+		print_char(cV);
+	if (dV < -2147483648.0 || dV > 2147483647.0)
+		std::cout << "int: " << IMPOSSIBLE << std::endl;
+	else
+		print_int(iV);
+	print_float(fV);
+	print_double(dV);
 }
 
 void	ScalarConverter::convert(std::string value)
 {
-	if (value.length() == 1)
-		return (convert_char(value[0]));
-	return (convert_not_char(value));
+	std::string trimmed = trim(value);
+	t_type t = identify(trimmed);
+	switch (t)
+	{
+	case TYPE_CHAR:
+		show_char(trimmed);
+		break;
+	case TYPE_INT:
+		show_int(trimmed);
+		break;
+	case TYPE_FLOAT:
+		show_float(trimmed);
+		break;
+	case TYPE_DOUBLE:
+		show_double(trimmed);
+		break;
+	default:
+		print_all_impossible();
+		return ;
+	}
 }
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: omoreno- <omoreno-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 16:13:40 by omoreno-          #+#    #+#             */
-/*   Updated: 2023/09/29 18:15:37 by omoreno-         ###   ########.fr       */
+/*   Updated: 2023/10/02 17:26:29 by omoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,40 @@
 class ScalarConverter
 {
 private:
+	typedef enum e_type
+	{
+		TYPE_INVALID,
+		TYPE_CHAR,
+		TYPE_INT,
+		TYPE_FLOAT,
+		TYPE_DOUBLE
+	}	t_type;
+	typedef struct s_counts
+	{
+		size_t alphas;
+		size_t digits;
+		size_t signs;
+		size_t exps;
+		size_t floats;
+		size_t dots;
+		size_t spaces;
+	}	t_counts;
 	ScalarConverter();
 	~ScalarConverter();
 	ScalarConverter(const ScalarConverter& b);
-	ScalarConverter& operator=(const ScalarConverter& b);
-	static void	convert_char(char value);
-	static void	convert_not_char(std::string value);
-	static bool convert_to_int(int *i, std::string value);
-	static bool convert_to_double(double *d, std::string value);
+	ScalarConverter& 	operator=(const ScalarConverter& b);
+	static void			print_char(char cV);
+	static void			print_int(int iV);
+	static void			print_float(float fV);
+	static void			print_double(double dV);
+	static void			print_all_impossible();
+	static void			show_char(std::string trimmed);
+	static void			show_int(std::string trimmed);
+	static void			show_float(std::string trimmed);
+	static void			show_double(std::string trimmed);
+	static std::string	trim(std::string value);
+	static t_counts		counts(std::string value);
+	static t_type		identify(std::string value);
 public:
 	static void	convert(std::string value);
 };
